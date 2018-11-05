588d5a138264f1295d2415463d46993856c51159
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-05 08:43:09 -0500

    Pass action object to store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,8 +1,13 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 10 }) => {
-  console.log("running");
-  return state;
+const store = createStore((state = { count: 10 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      count: state.count + 1
+    };
+  } else {
+    return state;
+  }
 }); //Set default state
 
 console.log(store.getState()); //Return current state object
@@ -10,8 +15,8 @@ console.log(store.getState()); //Return current state object
 // Actions -an object that gets sent to the store
 
 // Increment the count - dispatch sends action object to the store
-// store.dispatch({
-//   type: "INCREMENT"
-// });
+store.dispatch({
+  type: "INCREMENT"
+});
 
 console.log(store.getState());

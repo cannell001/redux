4facbe0f0d87e57840b3a1ff22b64188b9be9f15
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-05 08:20:33 -0500

    change state to 10

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 10 }) => {
   return state;
 }); //Set default state
 

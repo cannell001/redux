a4e8bb62c78e7e2cc8d7ad7351cf8adc15aaa659
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-05 11:34:49 -0500

    Decrement by 10

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -9,8 +9,10 @@ const store = createStore((state = { count: 10 }, action) => {
         count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case "RESET":
       return {
@@ -42,4 +44,9 @@ store.dispatch({
 
 store.dispatch({
   type: "DECREMENT"
+});
+
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
 });

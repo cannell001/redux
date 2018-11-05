b1132ff349c75d0e63b6a328e90cb970e81ad2c3
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-05 08:36:14 -0500

    supress dispatch

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -10,8 +10,8 @@ console.log(store.getState()); //Return current state object
 // Actions -an object that gets sent to the store
 
 // Increment the count - dispatch sends action object to the store
-store.dispatch({
-  type: "INCREMENT"
-});
+// store.dispatch({
+//   type: "INCREMENT"
+// });
 
 console.log(store.getState());

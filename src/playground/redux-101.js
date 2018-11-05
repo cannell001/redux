2b45efaaa320b4e72585b1137e82ee56baf4c22e
Jeremy Cannell <jcannell007@hotmail.com>
2018-11-05 08:32:54 -0500

    Dispatch an action

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,17 @@
 import { createStore } from "redux";
 
 const store = createStore((state = { count: 10 }) => {
+  console.log("running");
   return state;
 }); //Set default state
 
-console.log(store.getState()); //Return current stae object
+console.log(store.getState()); //Return current state object
 
-// Actions
-// I'd like to increment the count
-// I'd like to reset the count to zero
+// Actions -an object that gets sent to the store
+
+// Increment the count - dispatch sends action object to the store
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
